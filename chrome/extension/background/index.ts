78bcc8a48381838617035cf67b143389dbadf8b6
Derek Nordgren <derek.nordgren@protonmail.com>
2018-10-06 11:27:32 -0500

    Build background.js with Webpack & TypeScript.

--- chrome/extension/background/index.ts
+++ chrome/extension/background/index.ts
@@ -1,20 +1,20 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({ color: '#3aa757' }, function() {
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color: '#3aa757' }, () => {
     console.log('The color is green.');
   });
 });
 
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
   chrome.declarativeContent.onPageChanged.addRules([
     {
       conditions: [
         new chrome.declarativeContent.PageStateMatcher({
           pageUrl: { hostEquals: 'developer.chrome.com' },
-        })
+        }),
       ],
       actions: [
-        new chrome.declarativeContent.ShowPageAction()
-      ]
-    }
+        new chrome.declarativeContent.ShowPageAction(),
+      ],
+    },
   ]);
 });
